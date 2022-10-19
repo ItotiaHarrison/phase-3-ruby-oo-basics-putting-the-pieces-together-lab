ae9d00d9a23446524af922955320b9e0c263bb40
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-10-20 01:18:46 +0300

    commit tree 51793b88648eb9158668c97221792162462f4aea

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+attr_reader :title
+attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+Kasuku = Book.new("And Then There Were None")
+Kasuku = Book.new("Agatha Christie")
+Kasuku = Book.new(272)
+Kasuku = Book.new("Mystery")
+puts Kasuku.title 
+puts Kasuku.author
